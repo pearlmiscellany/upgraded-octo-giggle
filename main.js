5a97c5bcc915cdebe99416c53837336e7c603a8a
pearlmiscellany <pearlmiscellany@protonmail.com>
2021-08-30 20:32:01 +0700

    new

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!xx</h1>
+  <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+let rps = document.getElementById('rps');
+rps.addEventListener('click', (e) => {
+  console.log('rps');
+});
